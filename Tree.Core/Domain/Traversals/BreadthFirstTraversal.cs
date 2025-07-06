2c2d90b89ae0e0f926383736ad534c2f62325462
Gessio Mori <gessiomorin@gmail.com>
2025-07-06 11:17:23 -0300

    feat(core): implementar DFS e BFS

---
+++ Tree.Core/Domain/Traversals/BreadthFirstTraversal.cs
@@ -0,0 +1,27 @@
+﻿using Tree.Core.Domain.Interfaces;
+
+namespace Tree.Core.Domain.Traversals;
+
+public class BreadthFirstTraversal(IPrintStrategy printStrategy, IMonitor monitor) : TreeTraversalBase(printStrategy, monitor)
+{
+    public override void Traverse(Component root)
+    {
+        Queue<(Component, int)> queue = new();
+        queue.Enqueue((root, 0));
+
+        while (queue.Count > 0)
+        {
+            (Component node, int depth) = queue.Dequeue();
+
+            NotifyAndPrint(node, depth);
+
+            if (node is CompositeNode composite)
+            {
+                foreach (Component child in composite.Children)
+                {
+                    queue.Enqueue((child, depth + 1));
+                }
+            }
+        }
+    }
+}
---
+++ Tree.Core/Domain/Traversals/DepthFirstTraversal.cs
@@ -0,0 +1,27 @@
+﻿using Tree.Core.Domain.Interfaces;
+
+namespace Tree.Core.Domain.Traversals;
+
+public class DepthFirstTraversal(IPrintStrategy printStrategy, IMonitor monitor) : TreeTraversalBase(printStrategy, monitor)
+{
+    public override void Traverse(Component root)
+    {
+        Stack<(Component, int)> stack = new();
+        stack.Push((root, 0));
+
+        while (stack.Count > 0)
+        {
+            (Component node, int depth) = stack.Pop();
+
+            NotifyAndPrint(node, depth);
+
+            if (node is CompositeNode composite)
+            {
+                for (int i = composite.Children.Count - 1; i >= 0; i--)
+                {
+                    stack.Push((composite.Children[i], depth + 1));
+                }
+            }
+        }
+    }
+}
